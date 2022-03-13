5c4ff03014a4fdc9a8583fd16e43e953d7f404cb
Gaurab Banerjee <gbandyein@gmail.com>
2022-03-13 23:11:29 +0530

    add color to text

--- src/components/VideoJS/VideoJS.js
+++ src/components/VideoJS/VideoJS.js
@@ -8,9 +8,9 @@ export default class VideoJS extends Component {
                 <div className="container box">
                     <div className="row d-flex align-items-center">
                         <div className=" col-md-6 col-sm-0 col-0 ">
-                            <h1 className='cus-d2'>Healthy Medics, </h1>
-                            <h1 className='cus-d2'>Better Healthcare,</h1>
-                            <h1 className='cus-d2'>Improved Lives!</h1>
+                            <h1 className='cus-d2'><span style={{color: 'rgb(245, 95, 75)'}} >Healthy</span> Medics, </h1>
+                            <h1 className='cus-d2'>Better <span style={{color: 'rgb(245, 95, 75)'}} >Healthcare</span>,</h1>
+                            <h1 className='cus-d2'><span style={{color: 'rgb(245, 95, 75)'}} >Improved</span> Lives!</h1>
                         </div>
                         <div className=" col-md-6 col-sm-12 col-12 videowrapper" >
                             <iframe src="https://player.vimeo.com/video/493370943?h=3f98cb151a" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen ></iframe>
