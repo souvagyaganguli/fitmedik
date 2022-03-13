e1b541fa194128b0a9ad09143e78a461ebeb8c80
Gaurab Banerjee <gbandyein@gmail.com>
2022-03-13 23:06:35 +0530

    appscreens responsive

--- src/components/App_Screen/Appscreens.js
+++ src/components/App_Screen/Appscreens.js
@@ -4,6 +4,7 @@ import './appscreens.css'
 export default class Appscreens extends Component {
     render() {
         return (
+            <>
             <div className="content" id="appscreen">
                 <div className="div-text1">
                     <div className="div-text2">
@@ -30,6 +31,43 @@ export default class Appscreens extends Component {
                     </div>
                 </div>
             </div>
+            <div className="content" id="appscreen_mob">
+                <h5>mobile</h5>
+                <div>
+                    <h1 className="display-2 cus-d2 text-white" style={{fontWeight: "600"}}>IoT enabled Mobile app for medics</h1>
+                    <h5 className="txt-body text-white pt-4 pb-4 lh-base">Fitmedik ensures your health and well being of healthcare workers through a tailor-made mobile app which monitors their heart rate and sleep cycles through a wearable. </h5>
+                    <div className="div-1">
+                        <div className="div-2">
+                            <div className="div-3">
+                                <img style={{display: "none"}} src="/" id="pic1" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h1 className="display-2 cus-d2 text-white" style={{fontWeight: "600"}}>Monitor vulnerabilities</h1>
+                    <h5 className="txt-body text-white pt-4 pb-4 lh-base">Fitmedik make healthcare workers aware about how vulnerable their health profile is to a certain disease through various Artificial Intelligence and Machine Learning Algorithms. </h5>
+                    <div className="div-1">
+                        <div className="div-2">
+                            <div className="div-3">
+                                <img style={{display: "none"}} src="/" id="pic2" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div >
+                    <h1 className="display-2 cus-d2 text-white" style={{fontWeight: "600"}}>On-demand access to care</h1>
+                    <h5 className="txt-body text-white pt-4 pb-4 lh-base">Fitmedik provides access to best Mental health and well being experts to the healthcare workers. Through anonymity and confidentiality, we break the barriers which medics face as patients. </h5>
+                    <div className="div-1">
+                        <div className="div-2">
+                            <div className="div-3">
+                                <img style={{display: "none"}} src="/" id="pic3" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </>
         )
     }
 }
